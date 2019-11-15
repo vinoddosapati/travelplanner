688b4d8c1a5153cbd523b395ccb77a518e5f8876
Vinod <vdosapati@albany.edu>
2019-11-15 17:05:29 -0500

    inital db configs

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/testdb1';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 /* GET api listing. */
 router.get('/search', (req, res) => {
-  API_KEY = 'AIzaSyDcX_ABXVp1M_OkjvJDyAAhGPnJsvsZhXg';
-  baseApiUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=1500&type=restaurant&key=';
 
-  console.log('reuest '+req.query);
-  API = baseApiUrl+API_KEY+"&location="+req.query.location;
-  console.log('url: ' + API);
-  //console.log(req.query.name);
-
-  axios.get(`${API}`)
-    .then(posts => {
-      //console.log(posts.data)
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
-
-
-  //res.send('api works');
+  res.send('api works');
 });
 
 module.exports = router;
